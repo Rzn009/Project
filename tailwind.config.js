6f8c5563c360fa55d83ef212bf25c94efdeb9dea
Razan Muhammad Aysel <razanmaysel08@gmail.com>
2024-01-29 23:49:15 -0800

    new-12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"
-,"./node_modules/flowbite/**/*.js"
-],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      fontFamily:{
+        'quantum':['Quantum Flat Hollow (BRK)', sans-serif]
+      },
     },
   },
-  plugins: [ require('flowbite/plugin'), 
-  require('@tailwindcss/typography'),
-  
-],
-  
-}
-
+  plugins: [require('flowbite/plugin'), require('@tailwindcss/typography'), require('taos/plugin')],
+};
