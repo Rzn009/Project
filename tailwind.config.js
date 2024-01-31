246338cd45de0259608908537b4de22686f0b934
Razan Muhammad Aysel <razanmaysel08@gmail.com>
2024-01-30 23:29:57 -0800

    new-14

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
-      fontFamily: {
-        'quantum-flat-hollow': ['Quantum Flat Hollow (BRK)', 'sans-serif'],
-        'quantum-flat': ['Quantum Flat (BRK)', 'sans-serif'],
-        'quantum-round-hollow': ['Quantum Round Hollow (BRK)', 'sans-serif'],
-        'quantum-round': ['Quantum Round (BRK)', 'sans-serif'],
-        'quantum-taper': ['Quantum Taper (BRK)', 'sans-serif'],
-    },
       colors:{
         dark_purple:"#000235",
         btn_primary:"#7e7ee3",
