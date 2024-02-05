f5bf007ba9c9ed8fcdac11979a9e75e10ca60f37
Razan Muhammad Aysel <razanmaysel08@gmail.com>
2024-02-04 22:50:29 -0800

    new-22

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  darkMode : 'class',
   theme: {
     extend: {
       colors:{
         dark_purple:"#000235",
         btn_primary:"#81689D",
         heading_color:"#abd9d9",
+        background : "#4A00E0",
+        background1 : "#8E2DE2",
       },
     },
   },
