e9effd4b107e8777707c07b613942d3fe50d0291
Razan Muhammad Aysel <razanmaysel08@gmail.com>
2024-02-06 20:18:27 -0800

    n-20

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,25 @@ module.exports = {
   darkMode : 'class',
   theme: {
     extend: {
+      keyframes: {
+        neon: {
+          "0%": {
+            textShadow: "0 0 10px #00B9FC",
+          },
+          "100%": {
+            textShadow: "0 0 10px #ff0000",
+          },
+        },
       colors:{
         dark_purple:"#000235",
         btn_primary:"#81689D",
         heading_color:"#abd9d9",
         background : "#4A00E0",
         background1 : "#8E2DE2",
+        background2:"#82589F"
       },
     },
   },
   plugins: [require('flowbite/plugin'), require('@tailwindcss/typography'), require('taos/plugin')],
+}
 };
